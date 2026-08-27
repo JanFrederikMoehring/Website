c3ff1629b6286b41aa26e54c3b245c96cb6299a5
Jan-Frederik Möhring <freddymoe@gmx.de>
2026-08-27 15:56:52 +0200

    Add favicon link to website.php

--- website.php
+++ website.php
@@ -11,6 +11,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>favourite.de</title>
+    <link rel="icon" type="image/vnd.microsoft.icon" href="favicon.ico">
     <link href="https://fonts.googleapis.com/css2?family=Limelight&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="styl>
 </head>
