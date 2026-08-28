dd7fcc41734624fc5d0444bc4441002d182c99e7
Jan-Frederik Möhring <freddymoe@gmx.de>
2026-08-28 14:13:22 +0200

    Change title and enhance footer links
    Updated the title and added links to GitHub and LinkedIn.

--- index.php
+++ index.php
@@ -1,23 +1,31 @@
+<?php
+
+//error_reporting(E_ALL);
+//ini_set('display_errors', TRUE);
+//ini_set('display_startup_errors', TRUE);
+?>
+
 <!DOCTYPE html>
 <html lang="de">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>favourite.de</title>
+    <title>jan-frederik.com</title>
+        <link rel="icon" type="image/vnd.microsoft.icon" href="favicon.ico">
     <link href="https://fonts.googleapis.com/css2?family=Limelight&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 <body style="background-color: #1E434C;">
 <h1 style="color: #C99E10"> <p style="font-family: Limelight" p> What's your favourite food?
 <body>
-    
+
     <form method="post">
-    
+
         <input type="text" name="Food" value="" placeholder="Pizza...">
         <input type="submit" value="Send" id="idSubmit"
         style="background-color:#C99e10; font-family:Roboto; Border:1px">
     </form>
-    
+
 <h3 style="color: #C99E10"> <p style="font-family: Roboto" p>
 
     <?php
@@ -31,25 +39,21 @@
         $time . '.txt' ,
         $_POST['Food'] ,
     ) ;
-    }
-
-    $dir = 'C:\Users\***\Desktop\phpFiles';
-    $files = scandir($dir);
-
-    // Datentypen:
-    // String: 'Jesper'
-    // Integers: 123
-    // Floats: 123,12
-    // Booleans: true/false
-    // Arrays: [1, 2, 3] 
+        echo strrev($_POST['Food']) . ', really?';
+    } ;
     ?>
-
-    <ul>
-        <?php foreach($files as $file): ?>
-            <li><?= $file ?></li>
-            <li><?php echo $file ?></li>
-        <?php endforeach; ?>
-    </ul>
-  
-</body>
-</html>
+    <br>
+    _________________________________________________
+    <br>
+    <style>
+    a:visited, a:visited, a:hover, a:active, a:link {
+  color: white;
+  background-color: transparent;
+  text-decoration: underline;
+}
+</style>
+    <h4 style="color: white"> <p style="font-family: Limelight" p>
+    Visit my
+    <a href="https://github.com/JanFrederikMoehring" ­target="_blank"­>GitHub</a>
+    and my
+    <a href="https://www.linkedin.com/in/jan-frederik-m%C3%B6hring/" ­target="_blank"­>LinkedIn</a>
