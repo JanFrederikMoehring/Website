f444283945cb36cd4123dbae9afe27dee46ea344
Jan-Frederik Möhring <freddymoe@gmx.de>
2026-08-31 13:37:20 +0200

    Refactor website.php HTML and fix markup
    Clean up and reorganize website.php: move PHP block next to form handling, update page title to jan-frederik.com, fix Google Fonts rel attribute, remove duplicate <body> and stray/invalid tags, and tidy form markup (input/submit). Add details/progress section and improve heading/styles. Correct external link attributes (remove stray characters and set target="_blank"). Preserve existing behavior of echoing the reversed submitted food.

--- website.php
+++ website.php
@@ -1,59 +1,86 @@
-<?php
-
-//error_reporting(E_ALL);
-//ini_set('display_errors', TRUE);
-//ini_set('display_startup_errors', TRUE);
-?>
-
 <!DOCTYPE html>
 <html lang="de">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>favourite.de</title>
+    <title>jan-frederik.com</title>
+
     <link rel="icon" type="image/vnd.microsoft.icon" href="favicon.ico">
+
     <link href="https://fonts.googleapis.com/css2?family=Limelight&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="styl>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
+
 <body style="background-color: #1E434C;">
-<h1 style="color: #C99E10"> <p style="font-family: Limelight" p> What's your favourite food?
-<body>
 
-    <form method="post">
+<h1 style="color: #C99E10; font-family: Limelight;">
+    What's your favourite food?
+</h1>
 
-        <input type="text" name='Food' value="" placeholder="Pizza...">
-        <input type="submit" value="Send" id="idSubmit"
+<form method="post">
+
+    <input type="text" name="Food" value="" placeholder="Pizza...">
+
+    <input type="submit" value="Send" id="idSubmit"
         style="background-color:#C99e10; font-family:Roboto; Border:1px">
-    </form>
 
-<h3 style="color: #C99E10"> <p style="font-family: Roboto" p>
+</form>
 
-    <?php
+<h3 style="color: #C99E10; font-family: Roboto;">
 
-    $time = time();
+<?php
 
-    if (empty($_POST['Food'])) {
-        echo "Please fill in the gap now";
-    } else {
-        #file_put_contents(
-        #$time . '.txt' ,
-        #$_POST['Food'] ,
+$time = time();
+
+if (empty($_POST['Food'])) {
+    echo "Please fill in the gap now";
+} else {
+    #file_put_contents(
+    #$time . '.txt' ,
+    #$_POST['Food'] ,
     #) ;
-        echo strrev($_POST['Food']) . ', really?';
-    } ;
-    ?>
-    <br>
-    _________________________________________________
-    <br>
-    <style>
-    a:visited, a:visited, a:hover, a:active, a:link {
-  color: white;
-  background-color: transparent;
-  text-decoration: underline;
+    echo strrev($_POST['Food']) . ', really?';
 }
+
+?>
+
+<br>
+<br>
+_________________________________________________
+<br>
+<br>
+
+</h3>
+
+<details style="color: #C99E10; font-family: Roboto;">
+    <summary>Click here after entering your favourite food</summary>
+
+    <h5>
+        <progress value="75" max="100"
+            style="accent-color: #C99E10;"></progress>
+        80% of my website completed. Now just do what is written below.
+    </h5>
+</details>
+
+<br>
+
+<style>
+    a:visited, a:visited, a:hover, a:active, a:link {
+        color: white;
+        background-color: transparent;
+        text-decoration: underline;
+    }
 </style>
-    <h4 style="color: white"> <p style="font-family: Limelight" p>
+
+<h4 style="color: white; font-family: Limelight;">
+
     Visit my
-    <a href="https://github.com/JanFrederikMoehring" ­target="_blank"­>GitHub</a>
+    <a href="https://github.com/JanFrederikMoehring" target="_blank">GitHub</a>
     and my
-    <a href="https://www.linkedin.com/in/jan-frederik-m%C3%B6hring/" ­target="_blank"­>LinkedIn</a>
+    <a href="https://www.linkedin.com/in/jan-frederikM%C3%B6hring/" target="_blank">LinkedIn</a>
+
+</h4>
+
+</body>
+</html>
