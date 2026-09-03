e7886763c4b3404035e81e1045ec99d4090eb2b4
Jan-Frederik Möhring <freddymoe@gmx.de>
2026-09-03 11:37:59 +0200

    checke

---
+++ test.php
@@ -0,0 +1,8 @@
+<details>
+  <summary>Click here after entering your favourite food</summary>
+  <p>
+<progress value="75" max="100"
+style="accent-color: #C99E10;"></progress>
+80% of my website completed </br>
+Now just do what is written below</p>
+</details>
