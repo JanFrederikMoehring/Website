7c614196cf5d4be0557200342989e646a4375a46
Jan-Frederik Möhring <freddymoe@gmx.de>
2026-09-03 16:02:59 +0200

    brought newest features back

--- test.php
+++
@@ -1,8 +0,0 @@
-<details>
-  <summary>Click here after entering your favourite food</summary>
-  <p>
-<progress value="75" max="100"
-style="accent-color: #C99E10;"></progress>
-80% of my website completed </br>
-Now just do what is written below</p>
-</details>
