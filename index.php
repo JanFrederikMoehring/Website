3d21b9f12b4415aa03c2f2cc6dcc7d496faf8ad5
Jan-Frederik Möhring <freddymoe@gmx.de>
2026-09-09 15:50:43 +0200

    Clean up index page layout
    This commit reformats the markup in index.php for better readability and consistency. It fixes the surrounding HTML structure for the email form, details section, and progress area while keeping the page behavior unchanged.

--- index.php
+++ index.php
@@ -53,76 +53,88 @@
         }
 
         ?>
-    
+
         <br>
         <br>
         _________________________________________________
 
-         </h3>
-          <br>
+    </h3>
 
-        <form action='create.php' method='post'>
+    <br>
 
-            <input
-                type='text'
-                name='email'
-                value=''
-                placeholder='E-Mail (only Example)'
-            >
+    <form action='create.php' method='post'>
 
-            <input
-                type='password'
-                id='pwd'
-                name='password'
-                value=''
-                placeholder='Password'
-            >
+        <input
+            type='text'
+            name='email'
+            value=''
+            placeholder='E-Mail (only Example)'
+        >
 
-            <input
-                type='submit'
-                value='Send'
-                id='idSubmit'
-                style='background-color:#C99e10; font-family:Roboto; Border:1px'
-            >
-            </form>
+        <input
+            type='password'
+            id='pwd'
+            name='password'
+            value=''
+            placeholder='Password'
+        >
 
-            <?php
+        <input
+            type='submit'
+            value='Send'
+            id='idSubmit'
+            style='background-color:#C99e10; font-family:Roboto; Border:1px'
+        >
 
-            // Datenbank erstellen
-            $db = new PDO('sqlite:/var/www/database/database.sqlite');
+    </form>
 
-            $stmt = $db->query('SELECT email from users');
-            $emaillist = ($stmt->fetchAll(PDO::FETCH_COLUMN));
+    <?php
 
-            ?>
-            
-        <h5>
+    // Datenbank erstellen
+    $db = new PDO('sqlite:/var/www/database/database.sqlite');
+
+    $stmt = $db->query('SELECT email from users');
+    $emaillist = ($stmt->fetchAll(PDO::FETCH_COLUMN));
+
+    ?>
+
+    <h5>
+
         <details style='color: #C99E10; font-family: Roboto;'>
-        <summary>Click here to see the other example e-mails</summary>
-            <ul>
-                <?php foreach($emaillist as $customers): ?>
-                    <li><?php echo($customers) ?></li>
-                <?php endforeach; ?>
-            </ul>
-        
-        </details>
-                </h5>
 
-        <h3 style='color: #C99E10; font-family: Roboto;'>
+            <summary>Click here to see the other example e-mails</summary>
+
+            <ul>
+
+                <?php foreach($emaillist as $customers): ?>
+
+                    <li><?php echo($customers) ?></li>
+
+                <?php endforeach; ?>
+
+            </ul>
+
+        </details>
+
+    </h5>
+
+    <h3 style='color: #C99E10; font-family: Roboto;'>
+
         _________________________________________________
 
-                </h3>
-        <br>
+    </h3>
 
-    
-            <progress
-                value='75'
-                max='100'
-                style='accent-color: #C99E10;'
-            ></progress>
-            <h4 style='display: inline; color: #C99E10; font-family: Roboto;'>
-            80% of my website completed. Now just do what is written below.
-                </h4>
+    <br>
+
+    <progress
+        value='75'
+        max='100'
+        style='accent-color: #C99E10;'
+    ></progress>
+
+    <h4 style='display: inline; color: #C99E10; font-family: Roboto;'>
+        80% of my website completed. Now just do what is written below.
+    </h4>
 
     <br>
 
@@ -137,10 +149,13 @@
     <h4 style='color: white; font-family: Limelight;'>
 
         Visit my
+
         <a href='https://github.com/JanFrederikMoehring' target='_blank'>
             GitHub
         </a>
+
         and my
+
         <a href='https://www.linkedin.com/in/jan-frederik-möhring' target='_blank'>
             LinkedIn
         </a>
