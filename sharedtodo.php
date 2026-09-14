7f21552872250d5f3477fa3d5d35057e52eeb94f
Jan-Frederik Möhring <freddymoe@gmx.de>
2026-09-14 11:25:06 +0200

    Replace Font Awesome home icon with Unicode
    Replace the <i class="fa-solid fa-house"> Font Awesome home icon with a simple Unicode house (⌂) in sharedtodo.php and tweak the anchor's inline styles to use a text-shadow and adjusted font styling. This removes the dependency on the icon font for this link and simplifies rendering.

--- sharedtodo.php
+++ sharedtodo.php
@@ -6,7 +6,7 @@ $db = new PDO('sqlite:/var/www/database/todo.sqlite');
 
 ?>
 
-    <a href="/" style="text-decoration: none; color: #C99E10; font-size: 25px; font-weight: bold;">
-            <i class="fa-solid fa-house"></i>
+    <a href="/" style="text-decoration: none; color: #C99E10; font-size: 25px; text-shadow: 0.5px 0 #C99E10 -0.5px 0 #C99e10;">
+            ⌂
         </a>
 <br><br>
