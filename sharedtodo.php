4988bc8c5165f7ff22a06ae3ee4fb5a9ff4ed639
Jan-Frederik Möhring <freddymoe@gmx.de>
2026-09-14 11:25:47 +0200

    deleted shadow

--- sharedtodo.php
+++ sharedtodo.php
@@ -6,7 +6,7 @@ $db = new PDO('sqlite:/var/www/database/todo.sqlite');
 
 ?>
 
-    <a href="/" style="text-decoration: none; color: #C99E10; font-size: 25px; text-shadow: 0.5px 0 #C99E10 -0.5px 0 #C99e10;">
+    <a href="/" style="text-decoration: none; color: #C99E10; font-size: 25px;">
             ⌂
         </a>
 <br><br>
