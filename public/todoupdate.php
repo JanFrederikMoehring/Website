bf0d64277da1503bfa89595b61bffb2226e82a2d
Jan-Frederik Möhring <freddymoe@gmx.de>
2026-09-15 15:15:01 +0200

    Render todos list in todoupdate.php
    Add a PDO query and loop to fetch all todos and render them as table rows in public/todoupdate.php. Each row now outputs id, editable Title and Description, Date, a Status checkbox, and an edit link that includes the todo ID. This enables dynamic display of todo items on the page.

--- public/todoupdate.php
+++ public/todoupdate.php
@@ -4,7 +4,37 @@ include './../sharedhtml.php';
 $Number = $_GET['ID'];
 echo $Number;
 
+$stmt = $db->query('SELECT * FROM todos');
+
+while ($todo = $stmt->fetch(PDO::FETCH_ASSOC)) {
+
+    $checked = $todo['Status'];
+    $Number = $todo['id']
+
 ?>
+
+        <tr>
+            <td><?= $todo['id'] ?></td>
+
+            <td contenteditable="true"><?= $todo['Title'] ?></td>
+
+            <td contenteditable="true"><?= $todo['Description'] ?></td>
+
+            <td><?= $todo['Date'] ?></td>
+
+            <td>
+                <input
+                    type="checkbox" <?= $checked ?>
+                    id="Checkbox"
+                    name="Checkbox"
+                    value="true"
+                >
+            </td>
+            <td><a href="/todoupdate.php/?ID=<?= urlencode($Number)?>" style="text-decoration: none; color: #C99E10;">⌨</a></td>
+        </tr>
+
+<?php } ?>
+
 </body>
 
 </html>
