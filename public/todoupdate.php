d575c566fc0050c67183f09c9cdadf21f25c57cf
Jan-Frederik Möhring <freddymoe@gmx.de>
2026-09-15 15:20:38 +0200

    Update todos query and remove edit link
    Modify public/todoupdate.php: change the SELECT from 'WHERE 1' to 'WHERE id 1' (intended to restrict by id) and remove the per-row edit link cell from the table. Note: the new WHERE clause is missing an operator (e.g. '=') and will cause a SQL syntax error; it should be corrected to use 'WHERE id = ?' or a prepared query filtering by the provided ID.

--- public/todoupdate.php
+++ public/todoupdate.php
@@ -4,7 +4,7 @@ include './../sharedhtml.php';
 $Number = $_GET['ID'];
 echo $Number;
 
-$stmt = $db->query('SELECT * FROM todos WHERE 1');
+$stmt = $db->query('SELECT * FROM todos WHERE id 1');
 
 while ($todo = $stmt->fetch(PDO::FETCH_ASSOC)) {
 
@@ -30,7 +30,6 @@ while ($todo = $stmt->fetch(PDO::FETCH_ASSOC)) {
                     value="true"
                 >
             </td>
-            <td><a href="/todoupdate.php/?ID=<?= urlencode($Number)?>" style="text-decoration: none; color: #C99E10;">⌨</a></td>
         </tr>
 
 <?php } ?>
