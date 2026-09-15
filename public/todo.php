1b97a14d2feaa5208e2ebe6f080a15531853e8d1
Jan-Frederik Möhring <freddymoe@gmx.de>
2026-09-15 11:00:51 +0200

    fixed bug

--- public/todo.php
+++ public/todo.php
@@ -57,7 +57,8 @@ while ($todo = $stmt->fetch(PDO::FETCH_ASSOC)) {
                     name="Checkbox"
                     value="true"
                 >
-            </td>⌨</td>
+            </td>
+            <td>⌨</td>
         </tr>
 
 <?php
