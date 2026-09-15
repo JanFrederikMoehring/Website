e5453743538a987f56a01f529fd0095b8964e81d
Jan-Frederik Möhring <freddymoe@gmx.de>
2026-09-15 14:41:19 +0200

    fixed bug

--- public/todoupdate.php
+++ public/todoupdate.php
@@ -1,31 +1,4 @@
-<!DOCTYPE html>
-<html lang="de">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title>jan-frederik.com | To Do</title>
-
-    <link
-        rel="icon"
-        type="image/vnd.microsoft.icon"
-        href="favicon.ico"
-    >
-
-    <link
-        href="https://fonts.googleapis.com/css2?family=Limelight&display=swap"
-        rel="stylesheet"
-    >
-
-    <link
-        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
-        rel="stylesheet"
-    >
-</head>
-
-<body style="background-color: #1E434C;">
-
 <?php
-include './../:sharedtodo.php';
+include './../sharedtodo.php';
+include './../sharedhtml.php';
 ?>
