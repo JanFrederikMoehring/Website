b761ccb109db2537d213f1812eb302f342ea6211
Jan-Frederik Möhring <freddymoe@gmx.de>
2026-09-15 15:16:13 +0200

    added specification

--- public/todoupdate.php
+++ public/todoupdate.php
@@ -4,7 +4,7 @@ include './../sharedhtml.php';
 $Number = $_GET['ID'];
 echo $Number;
 
-$stmt = $db->query('SELECT * FROM todos');
+$stmt = $db->query('SELECT * FROM todos WHERE ($Number)');
 
 while ($todo = $stmt->fetch(PDO::FETCH_ASSOC)) {
 
