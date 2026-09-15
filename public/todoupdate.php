f6d107628e3d8a3e81af8708a3a9633c1590d21f
Jan-Frederik Möhring <freddymoe@gmx.de>
2026-09-15 15:23:18 +0200

    update

--- public/todoupdate.php
+++ public/todoupdate.php
@@ -4,7 +4,7 @@ include './../sharedhtml.php';
 $Number = $_GET['ID'];
 echo $Number;
 
-$stmt = $db->query('SELECT * FROM todos WHERE id 1');
+$stmt = $db->query('SELECT * FROM todos WHERE rowid (1)');
 
 while ($todo = $stmt->fetch(PDO::FETCH_ASSOC)) {
 
