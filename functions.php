7465c107a04bb5c16995e52c3894e73df4a951a6
Jan-Frederik Möhring <freddymoe@gmx.de>
2026-09-15 10:08:21 +0200

    Move TODO form to todocopy.php; make Title input transparent
    Remove the inline TODO form from public/todo.php and add a new todocopy.php page that contains the full TODO HTML (includes sharedtodo.php, functions.php), the create form, and the table with editable fields, checkbox status and an Update button. Also adjust the Title input in inputForm() (functions.php) to use a transparent background for consistent styling.

--- functions.php
+++ functions.php
@@ -38,6 +38,7 @@ function inputForm() { ?>
             id="Title"
             name="Title"
             placeholder="Title"
+            style="background: transparent;"
         >
 
         <br>
--- todocopy.php
+++ todocopy.php
@@ -33,6 +33,53 @@ require_once './../functions.php';
 
 ?>
 
+    <form
+        action="/todocreate.php"
+        method="post"
+        style="color: #C99E10; font-family: Roboto;"
+    >
+
+        <label for="Title">Title</label>
+
+        <input
+            type="text"
+            id="Title"
+            name="Title"
+            placeholder="Title"
+        >
+
+        <br>
+
+        <label for="Description">Description</label>
+
+        <input
+            type="text"
+            id="Description"
+            name="Description"
+            placeholder="Your Description"
+        >
+
+        <br>
+
+        <label for="Checkbox">Status</label>
+
+        <input
+            type="checkbox"
+            id="Checkbox"
+            name="Checkbox"
+        >
+
+        <br>
+
+        <input
+            type="submit"
+            value="Send"
+            id="submit"
+            style="background-color:#C99e10; font-family:Roboto; Border:1px"
+        >
+
+    </form>
+
     <table style="color: white;">
 
         <tr>
