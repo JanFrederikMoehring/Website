4867bce34be2c5e0acf34139a9eca660918d7bd3
Jan-Frederik Möhring <freddymoe@gmx.de>
2026-09-17 13:01:22 +0200

    deployment test

--- database.php
+++ database.php
@@ -7,6 +7,7 @@ $db = new PDO('sqlite:' . __DIR__ . '/public/database/database.sqlite');
 
 // $db = new PDO('sqlite:/var/www/html/public/database/database.sqlite');
 
+
 // Users Tabelle erstellen
 $db->query('CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
