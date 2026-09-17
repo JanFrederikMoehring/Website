5339a55a215f4ff935e6fee4ffd2a5b56b090c5e
Jan-Frederik Möhring <freddymoe@gmx.de>
2026-09-17 10:38:16 +0200

    changed path

--- database.php
+++ database.php
@@ -3,7 +3,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$db = new PDO('sqlite:' . __DIR__ . 'database.sqlite');
+$db = new PDO('sqlite:' . __DIR__ . '/public/database.sqlite');
 
 // Users Tabelle erstellen
 $db->query('CREATE TABLE IF NOT EXISTS users (
